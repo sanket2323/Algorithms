8b52d1e7f3d8b83d19992a18f6ca3f582dedb45c
Sanket Mane <111991599+Sanketmane2323@users.noreply.github.com>
2023-07-14 16:57:41 +0530

    Maths concepts

---
+++ MathsforDsa/src/sevieOfEratosthenes.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class sevieOfEratosthenes {
+    public static void main(String[] args) {
+        boolean isPrime[] = method(20);
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(i + " " + isPrime[i]);
+        }
+    }
+
+    static boolean[] method(int n) {
+        boolean[] arr = new boolean[n + 1];
+
+        Arrays.fill(arr, true);
+        arr[0] = false;
+        arr[1] = false;
+
+        for (int i = 2; i * i <= n; i++) {
+
+
+            for (int j = i*2; j <= n; j += i) {
+                arr[j] = false;
+            }
+        }
+
+        return arr;
+    }
+}
