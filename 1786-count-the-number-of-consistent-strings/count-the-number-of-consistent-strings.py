1f2864f4985e34908dc4af9e15a171e136f25b34
Sanket Mane <111991599+sanket2323@users.noreply.github.com>
2026-07-09 21:58:42 +1000

    Time: 218 ms (51.24%) | Memory: 21.1 MB (10.42%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
@@ -1,19 +1,33 @@
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
-        hash_list = [0] * 26
+        # hash_list = [0] * 26
+        # count = len(words)
+        # for ch in allowed:
+        #     char_index = ord(ch) - ord('a')
+        #     hash_list[char_index] = 1
+
+        # for word in words:
+        #     for ch in word:
+        #         ch_index = ord(ch) - ord('a')
+
+        #         if hash_list[ch_index] == 0:
+        #             count -= 1
+        #             break
+
+        # return count
+
+        # one more solution
+
         count = len(words)
-        for ch in allowed:
-            char_index = ord(ch) - ord('a')
-            hash_list[char_index] = 1
+        allowed_set = set(allowed)
 
         for word in words:
             for ch in word:
-                ch_index = ord(ch) - ord('a')
 
-                if hash_list[ch_index] == 0:
+                if ch not in allowed_set:
                     count -= 1
                     break
 
-        return count
+        return count 
 
         
